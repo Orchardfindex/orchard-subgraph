0a4bfdabc1ce02bfc8c681c072a1d49367e2a6e5
viet-nv <vietnv1304@gmail.com>
2022-02-08 16:38:57 +0700

    feat: add arbitrum network

--- src/config/constants/index.ts
+++ src/config/constants/index.ts
@@ -4,29 +4,29 @@ export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
 
 export const ADDRESS_LOCK = '0xffffffffffffffffffffffffffffffffffffffff'
 
-export const FACTORY_ADDRESS = '0x833e4083b7ae46cea85695c4f7ed25cdad8886de'
+export const FACTORY_ADDRESS = '0x570797C68C6D060503b2CfcBe5780205057010D5'
 
-export const ETH_PRICING_POOLS = '0xd478953d5572f829f457a5052580cbeaee36c1aa|0x20d6b227f4a5a2a13d520329f01bb1f8f9d2d628|0xf8467ef9de03e83b5a778ac858ea5c2d1fc47188|0xce9874c42dce7fffbe5e48b026ff1182733266cb'
+export const ETH_PRICING_POOLS = '0x8d040883136957c264c4cf51a8ea9b8dfb19eee7'
 
 // minimum liquidity required to count towards tracked volume for pairs with small # of Lps
-export let MINIMUM_USD_THRESHOLD_NEW_PAIRS = BigDecimal.fromString('4000')
+export let MINIMUM_USD_THRESHOLD_NEW_PAIRS = BigDecimal.fromString('0')
 
 // minimum liquidity for price to get tracked
-export let MINIMUM_LIQUIDITY_THRESHOLD_ETH = BigDecimal.fromString('2') // default is 2
+export let MINIMUM_LIQUIDITY_THRESHOLD_ETH = BigDecimal.fromString('0') // default is 2
 
-export const WETH_ADDRESS = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'
-export const DAI_ADDRESS = '0x6b175474e89094c44da98b954eedeac495271d0f'
-export const USDC_ADDRESS = '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48'
-export const USDT_ADDRESS = '0xdac17f958d2ee523a2206206994597c13d831ec7'
+export const WETH_ADDRESS = '0x93D51226c4C3c265ca0c2F6420fa4c6A9151c09e'
+export const DAI_ADDRESS = '0x340eefA7154BF23e328F59E936568607841FE027'
+export const USDC_ADDRESS = '0xB4BbD0250618917b3679578C58f6440A227C8c03'
+export const USDT_ADDRESS = '0x41a56c30b881296859FB4db30Eb9a639B473619B'
 
-export const KNC_ADDRESS = '0xdefa4e8a7bcba345f687a2f1456f5edd9ce97202'
+export const KNC_ADDRESS = '0x7596961744096D12eFa3CfA58d1D30EDd82BD396'
 export const KNC_NAME = 'Kyber Network'
 export const KNC_SYMBOL = 'KNC'
 
-export const KNCL_ADDRESS = '0xdd974d5c2e2928dea5f71b9825b8b646686bd200'
+export const KNCL_ADDRESS = '0x0000000000000000000000000000000000000000'
 export const KNCL_NAME = 'Kyber Network Legacy'
 export const KNCL_SYMBOL = 'KNCL'
 
 export let FACTORY_BPS = BigDecimal.fromString('10000')
 
-export let NETWORK = 'mainnet'
+export let NETWORK = 'arbitrum-rinkeby'
